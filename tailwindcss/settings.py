c14a707cddb934eee4bfa0f25e96c49a514d2480
Luca Liehner <lliehner@Lucas-MBP-2.fritz.box>
2021-10-21 19:18:15 +0200

    Tailwind is installed

--- tailwindcss/settings.py
+++ tailwindcss/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'theme',
     'tailwind',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +125,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
